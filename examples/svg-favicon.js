bc9da22c92051b0550526015e13b5efac49cf61b
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-04-15 15:42:38 +0200

    svg-favicon.js update, using event.waitUntil

--- examples/svg-favicon.js
+++ examples/svg-favicon.js
@@ -1,7 +1,19 @@
+let path, origin, toCache;
 
 addEventListener("fetch", event => {
 	event.respondWith(handleRequest(event, event.request));
+	//if(toCache)event.waitUntil(cacheRequest(event, event.request));
 });
+
+
+/**
+ * 
+ * @param {Event} event - Event
+ * @returns {Promise} Promise
+ */
+async function cacheRequest(event){
+	await KV.put(path, await toCache.arrayBuffer());
+}
 
 /**
  * 
@@ -10,7 +22,7 @@ addEventListener("fetch", event => {
  * @returns {Promise<Response>} Promised Response
  */
 async function handleRequest(event, request){
-	const {pathname: path, origin} = new URL(request.url);
+	{pathname: path, origin} = new URL(request.url);
 	if(path.startsWith('/svg/')){
 		return new Response(`<svg width="128" height="128"><text x="20" y="35">${path.slice(5)}</text></svg>`, {headers: {'Content-Type': 'image/svg+xml', 'X-Type': 'generated'}});
 	} else {
@@ -19,7 +31,9 @@ async function handleRequest(event, request){
 
 		let a = await fetch(`https://images.weserv.nl/?output=png&url=${origin}/svg/${path.slice(1)}`);
 		let obj={};for(let pair of a.headers.entries()){obj[pair[0]]=pair[1]}
-		await KV.put(path, await a.clone().arrayBuffer()/*, {metadata: obj}*/);
+		toCache = a.clone();
+		event.waitUntil(cacheRequest(event));
+		//await KV.put(path, await a.clone().arrayBuffer()/*, {metadata: obj}*/);
 		//a.headers.set('X-Type', 'fetched');
 		//return a;
 		return new Response(a.body, {headers: {...a.headers, 'X-Type': 'fetched'}});
